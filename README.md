07e358cf8762a934b6184c20da928e9345d72fda
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-05-23 08:04:41 +0200

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+## Install
+Copy env file and install dependencies.
+```bash
+cp .env.example .env
+npm install
+```
+
+## Usage
+```bash
+npm start
+```
+
+go to http://localhost:3000
---
+++ index.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import Stripe from 'stripe';
+import express from 'express';
+
+const port = 3000;
+const domain = process.env.DOMAIN;
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/create-checkout-session', async (req, res) => {
+    const { quantity } = req.body;
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        { price: process.env.TEST_PRODUCT_ID, quantity },
+      ],
+      mode: 'payment',
+      success_url: `${domain}/success.html`,
+      cancel_url: `${domain}/cancel.html`,
+    });
+  
+    res.redirect(303, session.url);
+  });
+
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+});
+
+// Docs: https://docs.stripe.com/api/payment_intents/object
+// Repo: https://github.com/niiicolai/10b_integrate_with_payment
+// https://docs.stripe.com/payments/checkout
